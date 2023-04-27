5d7b50beaffdcf4ae813849b1a4f6b1a4ca6e4ca
Augusto Romero <augustoromerodev@gmail.com>
2023-04-27 12:05:00 -0300

    Redux Store Setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import store from "./src/store/store";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+          </View>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/store/gameSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Game } from "../types/Game";
+
+interface GameState {
+  games: Game[];
+}
+
+const initialState: GameState = {
+  games: [],
+};
+
+const gameSlice = createSlice({
+  name: "game",
+  initialState,
+  reducers: {
+    setGames: (state, action: PayloadAction<Game[]>) => {
+      state.games = action.payload;
+    },
+  },
+});
+
+export const { setGames } = gameSlice.actions;
+
+export default gameSlice.reducer;
